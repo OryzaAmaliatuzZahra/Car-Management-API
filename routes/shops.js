e6caba253c5ab4e1710c33e5f8b3a3f39234df8d
OryzaAmaliatuzZahra <127116057+OryzaAmaliatuzZahra@users.noreply.github.com>
2023-05-07 00:10:16 +0700

    challenge 5 commit

---
+++ routes/shops.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const shopController = require('../controller/shopController');
+const Auth = require('../middleware/auth');
+const checkRole = require('../middleware/checkRole');
+
+router.get('/', Auth, shopController.getShops)
+router.get('/search', Auth, shopController.searchShops)
+router.get('/:id', Auth, shopController.getShopById)
+router.put('/:id', Auth, checkRole('admin'), shopController.editShop)
+router.delete('/:id', Auth, checkRole('superadmin'), shopController.deleteShop)
+router.post('/create', Auth, checkRole('user'), shopController.createShop)
+
+module.exports = router
