dce7ab372fd70ae98d5d2c560b4c3d96d288e9ae
OryzaAmaliatuzZahra <127116057+OryzaAmaliatuzZahra@users.noreply.github.com>
2023-05-07 00:10:53 +0700

    challenge 5 commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const userController = require('../controller/userController');
+const Auth = require('../middleware/auth');
+const checkRole = require('../middleware/checkRole');
+
+
+router.get('/', Auth, userController.getUsers)
+router.post('/register', userController.createUser)
+router.post('/login', userController.login)
+router.get('/:id', Auth, checkRole('admin'), userController.getUserById)
+router.put('/:id', Auth, checkRole('user'), userController.editUser)
+router.delete('/:id', Auth, checkRole('superadmin'), userController.deleteUser)
+
+module.exports = router
