fc14ed82a7eefa8c14fad8d13f84b872e4ccc9f2
OryzaAmaliatuzZahra <127116057+OryzaAmaliatuzZahra@users.noreply.github.com>
2023-05-07 15:39:27 +0700

    Update shops.js

--- routes/shops.js
+++ routes/shops.js
@@ -6,6 +6,7 @@ const checkRole = require('../middleware/checkRole');
 
 router.get('/', shopController.getShops)
 router.get('/:id', Auth, shopController.getShopById)
+router.get('/search', shopController.searchShops)
 router.put('/:id', Auth, checkRole('admin'), shopController.editShop)
 router.delete('/:id', Auth, checkRole('superadmin'), shopController.deleteShop)
 router.post('/create', Auth, checkRole('user'), shopController.createShop)
